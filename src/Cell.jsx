70e552a2726397d09a152e5dd33d3a1c5ed46762
1F10240198 <s1f102401988@iniad.org>
2026-01-11 14:05:09 +0900

    Flagを立てる機能を追加

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -1,12 +1,20 @@
-const Cell = ({details}) => {
+const Cell = ({details, onUpdateFlag}) => {
     const cellStyle = {
         width: 40, height: 40, background: "lightgrey",
         borderWidth: 3, borderStyle: "outset", display: "flex",
         justifyContent: "center", alignItems: "center", cursor: "pointer"
     }
+
+    const getCellDisplay = () => {
+        if(!details.revealed) {return details.flagged ? '🚩': null;}
+        if(details.value === 'X') {return "💣"}
+        if(details.value === 0) {return null;}
+        return details.value;
+    }
+    
     return (
-        <div style={cellStyle}>
-            {details.value}
+        <div style={cellStyle} onContextMenu={onUpdateFlag}>
+            {getCellDisplay()}
         </div>
     );
 }
