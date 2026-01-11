3ed689a5386b88a29305e93daeba6eb5e42b15de
1F10240198 <s1f102401988@iniad.org>
2026-01-11 13:33:59 +0900

    Cell.jsxを作成

---
+++ src/Cell.jsx
@@ -0,0 +1,14 @@
+const Cell = ({details}) => {
+    const cellStyle = {
+        width: 40, height: 40, background: "lightgrey",
+        borderWidth: 3, borderStyle: "outset", display: "flex",
+        justifyContent: "center", alignItems: "center", cursor: "pointer"
+    }
+    return (
+        <div style={cellStyle}>
+            {details.value}
+        </div>
+    );
+}
+
+export default Cell;
